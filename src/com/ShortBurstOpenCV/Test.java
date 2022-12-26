1fb7605d0d63bbc8d94c0dcbe957ad109e509e5b
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-12-26 13:12:04 +0100

    faces test

--- src/com/ShortBurstOpenCV/Test.java
+++ src/com/ShortBurstOpenCV/Test.java
@@ -1,24 +1,31 @@
 package com.ShortBurstOpenCV;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class Test {
     public static void main(String[] args) {
 
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
+        String imgFile = "images/sopranos.jpg";
+        Mat src = Imgcodecs.imread(imgFile);
 
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+        String xmlFile = "xml/lbpcascade_frontalface.xml";
+        CascadeClassifier cc = new CascadeClassifier(xmlFile);
+
+        MatOfRect faceDetection = new MatOfRect();
+        cc.detectMultiScale(src, faceDetection);
+
+        System.out.printf("Detected faces: %d%n", faceDetection.toArray().length);
+
+        for (Rect rect : faceDetection.toArray()) {
+            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+        }
+
+        Imgcodecs.imwrite("images/sopranos_out.jpg", src);
 
     }
 }
