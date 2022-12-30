c2db7fad034102fb78e5ff8ed68769629e2fe1c5
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-12-30 14:46:47 +0100

    small refactor 2

---
+++ src/com/ShortBurstOpenCV/Main.java
@@ -0,0 +1,11 @@
+package com.ShortBurstOpenCV;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FaceFinder faceFinder = new FaceFinder();
+        faceFinder.showMenu();
+    }
+
+
+}
