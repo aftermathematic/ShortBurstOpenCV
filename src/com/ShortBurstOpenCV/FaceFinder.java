48f3341efa448219eda6e5c5e86c03e675a7c78b
JAN VERMEERBERGEN <JAN.VERMEERBERGEN@vdabcampus.be>
2022-12-31 12:53:04 +0100

    jpg filter

--- src/com/ShortBurstOpenCV/FaceFinder.java
+++ src/com/ShortBurstOpenCV/FaceFinder.java
@@ -1,3 +1,10 @@
+/* *********************************************************************************
+ * Name: Jan Vermeerbergen
+ * Assignment: Java short burst project: Face detection using the OpenCV library
+ * School: Erasmushogeschool, Brussel
+ * *********************************************************************************
+ */
+
 package com.ShortBurstOpenCV;
 
 import org.opencv.core.Point;
@@ -6,28 +13,23 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class FaceFinder {
 
-    File file = null;
-
-
-    public void showMenu(){
-
+    public void showMenu() {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println();
             System.out.println("Menu: ");
-            System.out.println("1. Load new image");
+            System.out.println("1. Detect faces in image");
+            System.out.println("2. Detect eyes in image");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
 
@@ -36,13 +38,26 @@ public class FaceFinder {
                 case 1:
                     try {
                         // Get image file
-                        file = GetFile();
+                        File file = getFile();
                         // Detect faces in file
-                        detectFaces(file);
+                        detectFeatures(file, "xml/lbpcascade_frontalface.xml", "faces", true);
                     } catch (IOException e) {
                         System.out.println("Error: " + e.getMessage());
                     }
                     break;
+
+                case 2:
+                    try {
+                        // Get image file
+                        File file = getFile();
+                        // Detect faces in file
+                        detectFeatures(file, "xml/haar_two_eyes_big.xml", "eyes", false);
+                    } catch (IOException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                    break;
+
+
                 case 0:
                     // Exit the program
                     return;
@@ -51,84 +66,85 @@ public class FaceFinder {
                     break;
             }
         }
-
     }
-    private void detectFaces(File file) {
 
+    private void detectFeatures(File file, String path, String featureType, boolean resize) throws IOException {
+
+        // Load OpenCV library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        ArrayList<String> ar = new ArrayList<>();
+        String filename = file.getName();
+        // Generate a timestamp and a filename for the face image
+        long timestamp = System.currentTimeMillis() / 1000L;
 
-        try {
-            String imgFile = saveImage(file);
+        Mat src = Imgcodecs.imread(file.getAbsolutePath());
+        CascadeClassifier cc = new CascadeClassifier(path);
 
-            Mat src = Imgcodecs.imread(imgFile);
+        MatOfRect featureDetection = new MatOfRect();
 
-            String xmlFile = "xml/lbpcascade_frontalface.xml";
-            CascadeClassifier cc = new CascadeClassifier(xmlFile);
+        // Detect faces in loaded image
+        cc.detectMultiScale(src, featureDetection);
 
-            MatOfRect faceDetection = new MatOfRect();
+        // Create an executor to handle multithreading
+        ExecutorService executor = Executors.newFixedThreadPool(8);
 
-            // Detect faces in loaded image
-            cc.detectMultiScale(src, faceDetection);
+        int count = 1;
+        System.out.println();
+        for (Rect rect: featureDetection.toArray()) {
+            // Draw a rectangle around the detected face
+            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
 
-            // Create an executor to handle multithreading
-            ExecutorService executor = Executors.newFixedThreadPool(8);
+            // Crop the face image from the source image
+            Mat faceFeature = src.submat(rect);
 
-            int count = 1;
-            for (Rect rect : faceDetection.toArray()) {
-                // Draw a rectangle around the detected face
-                Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
-
-                // Crop the face image from the source image
-                Mat face = src.submat(rect);
-
+            if(resize){
                 // Convert the face image to grayscale
-                Mat faceGrayscale = new Mat();
-                Imgproc.cvtColor(face, faceGrayscale, Imgproc.COLOR_BGR2GRAY);
-
+                Mat faceFeatureGrayscale = new Mat();
+                Imgproc.cvtColor(faceFeature, faceFeatureGrayscale, Imgproc.COLOR_BGR2GRAY);
                 // Resize the face image to 150x150 pixels
                 Mat faceResized = new Mat();
                 Size size = new Size(150, 150);
-                Imgproc.resize(faceGrayscale, faceResized, size);
-
-                // Generate a timestamp and a filename for the face image
-                long timestamp = System.currentTimeMillis() / 1000L;
-                String filename = "images/faces/face_" + timestamp + "_" + String.format("%03d", count) + ".jpg";
-                //faces.add(filename);
-
-                // Create a Runnable task to save the face image in a separate thread
-                int finalCount = count;
-                Runnable saveTask = () -> {
-                    // Save the face image to a file
-                    Imgcodecs.imwrite(filename, faceResized);
-                    System.out.println("Saved face #" + String.format("%03d", finalCount) + ": " + filename);
-                    ar.add(filename);
-                };
-
-                // Execute the save task in a separate thread
-                executor.execute(saveTask);
-
-                count++;
+                Imgproc.resize(faceFeatureGrayscale, faceResized, size);
             }
 
-            // Shutdown the executor
-            executor.shutdown();
+            // Find the index of the last dot and the extension of the file
+            int lastDotIndex = filename.lastIndexOf('.');
+            String extension = filename.substring(lastDotIndex);
 
-            System.out.println();
-            ar.forEach(System.out::println);
+            String featureName = "images/" + featureType + "/" + filename.substring(0, lastDotIndex) + "_" + timestamp + "_" + String.format("%03d", count) + extension;
 
+            // Create a Runnable task to save the face image in a separate thread
+            int finalCount = count;
+            Runnable saveTask = () -> {
+                    // Save the face or eyes image to a file
+                    Imgcodecs.imwrite(featureName, faceFeature);
+            System.out.println("Saved #" + String.format("%03d", finalCount) + ": " + featureName);
+            };
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            // Execute the save task in a separate thread
+            executor.execute(saveTask);
+
+            count++;
         }
 
+        // Shutdown the executor
+        executor.shutdown();
+
+        // Find the index of the last dot and the extension of the file
+        int lastDotIndex = filename.lastIndexOf('.');
+        String extension = filename.substring(lastDotIndex);
+
+        // build the modified filename
+        String newFilenameOut = "images/photos/" + filename.substring(0, lastDotIndex) + timestamp + "-out" + extension;
+
+        // Write the updated file with all faces marked
+        Imgcodecs.imwrite(newFilenameOut, src);
 
     }
 
-    private File GetFile() throws IOException {
+    private File getFile() throws IOException {
         // Open a file chooser dialog to select an image file
-        FileDialog fileDialog = new FileDialog((Frame)null, "Select an image file");
+        FileDialog fileDialog = new FileDialog((Frame)null, "Select a JPG image file");
         fileDialog.setMode(FileDialog.LOAD);
         fileDialog.setVisible(true);
 
@@ -138,30 +154,15 @@ public class FaceFinder {
             throw new IOException("No file was selected");
         }
         String dirPath = fileDialog.getDirectory();
+        File file = new File(dirPath + filePath);
 
-        return new File(dirPath + filePath);
+        // Check if the selected file has a JPG extension
+        if (!file.getName().toLowerCase().endsWith(".jpg") && !file.getName().toLowerCase().endsWith(".jpeg")) {
+            throw new IOException("Selected file is not a JPG image");
+        }
+
+        return file;
     }
-
-    public String saveImage(File file) throws IOException {
-
-        String filename = file.getName();
-
-        // Get the current Unix timestamp
-        long timestamp = System.currentTimeMillis() / 1000L;
-
-        // Find the index of the last dot and the extension
-        int lastDotIndex = filename.lastIndexOf('.');
-        String extension = filename.substring(lastDotIndex);
-
-        // build the modified filename
-        String newFilename_out = filename.substring(0, lastDotIndex) + "-" + timestamp + extension;
-
-        // Save the image files to the "images" directory
-        ImageIO.write(ImageIO.read(file), "jpg", new File("images/" + newFilename_out));
-
-        return "images/" + newFilename_out;
-    }
-
 
 
 }
--- src/com/ShortBurstOpenCV/Main.java
+++ src/com/ShortBurstOpenCV/Main.java
@@ -1,9 +1,19 @@
+/* *********************************************************************************
+ * Name: Jan Vermeerbergen
+ * Assignment: Java short burst project: Face detection using the OpenCV library
+ * School: Erasmushogeschool, Brussel
+ * *********************************************************************************
+ */
+
 package com.ShortBurstOpenCV;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Create an instance of the FaceFinder class
         FaceFinder faceFinder = new FaceFinder();
+
+        // Call the showMenu method on the faceFinder instance
         faceFinder.showMenu();
     }
 
